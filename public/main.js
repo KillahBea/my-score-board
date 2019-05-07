277e06d7cc443b1582871f14de307cf0ca08ec44
KillahBea <bee.noelle6@gmail.com>
2019-05-06 21:34:30 -0400

    working buttons

--- public/main.js
+++ public/main.js
@@ -1,7 +1,40 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
-  }
+let team1Score = 0
+
+const addPoint = () => {
+  team1Score += 1
+  // update the score in HTML
+  document.querySelector('.team1Score').textContent = team1Score
 }
+
+const subtractPoint = () => {
+  team1Score -= 1
+  // update the score in HTML
+  document.querySelector('.team1Score').textContent = team1Score
+}
+
+const main = () => {
+  console.log('hello there!')
+}
+
+let team2Score = 0
+
+const add2Point = () => {
+  team2Score += 1
+  document.querySelector('.team2Score').textContent = team2Score
+}
+
+const subtract2Point = () => {
+  team2Score -= 1
+  document.querySelector('.team2Score').textContent = team2Score
+}
+
+document.querySelector('.team-1-add-1-button').addEventListener('click', addPoint)
+document.querySelector('.team-1-subtract-1-button').addEventListener('click', subtractPoint)
+
+// document.querySelector('.add-1').addEventListener('click', addPoint)
+// document.querySelector('subtract-1').addEventListener('click', subtractPoint)
+
+document.querySelector('.add-button').addEventListener('click', add2Point)
+document.querySelector('.subtract-button').addEventListener('click', subtract2Point)
 
 document.addEventListener('DOMContentLoaded', main)
