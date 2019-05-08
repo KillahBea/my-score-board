b19f9d8629ab2a93c949209153213ea72235d072
KillahBea <bee.noelle6@gmail.com>
2019-05-07 21:23:48 -0400

    reset button

--- public/main.js
+++ public/main.js
@@ -4,10 +4,13 @@ const addPoint = () => {
   team1Score += 1
   // update the score in HTML
   document.querySelector('.team1Score').textContent = team1Score
+  if (team1Score >= 21) {
+    document.querySelector('.team1Score').textContent = 'You Win!!'
+  }
 }
 
 const subtractPoint = () => {
-  team1Score -= 1
+  if (team1Score > 0) team1Score -= 1
   // update the score in HTML
   document.querySelector('.team1Score').textContent = team1Score
 }
@@ -23,13 +26,19 @@ const main = () => {
 }
 
 let team2Score = 0
+
 const add2Point = () => {
   team2Score += 1
   document.querySelector('.team2Score').textContent = team2Score
+  if (team2Score >= 21) {
+    document.querySelector('.team2Score').textContent = 'You Win!!'
+  }
 }
 
 const subtract2Point = () => {
-  team2Score -= 1
+  if (team2Score > 0) {
+    team2Score -= 1
+  }
   document.querySelector('.team2Score').textContent = team2Score
 }
 
@@ -39,6 +48,13 @@ const updateTeam2Name = () => {
   team2Name.textContent = updateTeam2Name.value
 }
 
+const resetScores = () => {
+  team1Score = 0
+  document.querySelector('.team1Score').textContent = team1Score
+  team2Score = 0
+  document.querySelector('.team2Score').textContent = team2Score
+}
+
 document.querySelector('.team-1-add-1-button').addEventListener('click', addPoint)
 document.querySelector('.team-1-subtract-1-button').addEventListener('click', subtractPoint)
 document.querySelector('.update-team-1-name').addEventListener('click', updateTeam1Name)
@@ -46,5 +62,5 @@ document.querySelector('.update-team-1-name').addEventListener('click', updateTe
 document.querySelector('.add-button').addEventListener('click', add2Point)
 document.querySelector('.subtract-button').addEventListener('click', subtract2Point)
 document.querySelector('.update-team-2-name').addEventListener('click', updateTeam2Name)
-
+document.querySelector('.reset-button').addEventListener('click', resetScores)
 document.addEventListener('DOMContentLoaded', main)
